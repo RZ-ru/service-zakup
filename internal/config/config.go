5b4b52dffa8b9ade781a299f2e643c2161f2ed01
Roman <roman.zakruzhnoy@gmail.com>
2026-03-14 01:46:54 +0700

    added config

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	HTTPAddress      string
+	PostgresDSN      string
+	RabbitMQURL      string
+	RabbitMQExchange string
+}
+
+func MustLoad() *Config {
+	cfg := &Config{
+		HTTPAddress:      getEnv("HTTP_ADDRESS", ":8080"),
+		PostgresDSN:      getEnv("POSTGRES_DSN", ""),
+		RabbitMQURL:      getEnv("RABBITMQ_URL", ""),
+		RabbitMQExchange: getEnv("RABBITMQ_EXCHANGE", "application.events"),
+	}
+
+	if cfg.PostgresDSN == "" {
+		log.Fatal("Postgress_DSN is required")
+	}
+	if cfg.RabbitMQURL == "" {
+		log.Fatal("RabbitMQ_URL is required")
+	}
+	return cfg
+}
+
+func getEnv(key string, defaulValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaulValue
+	}
+	return value
+}
